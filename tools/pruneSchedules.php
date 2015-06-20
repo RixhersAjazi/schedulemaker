dc9450f2a384073015ee7f9680e71c1a7f8a86c9
RixhersAjazi <coderrix@gmail.com>
2015-06-19 22:32:16 -0400

    updated to pdo

--- tools/pruneSchedules.php
+++ tools/pruneSchedules.php
@@ -22,13 +22,16 @@ $where = "WHERE (NOW() - datelastaccessed > (60 * 60 * 24 * 90))";
 // Run the query to delete the courses of the schedule that are older than
 // 90 days
 $ninetyDaysAgo = date("Y-m-d H:i:s", strtotime("-90 days"));
-$query = "DELETE FROM schedules WHERE datelastaccessed < '{$ninetyDaysAgo}'";
-$result = mysql_query($query, $dbConn);
-if(!$result) {
+$query = "DELETE FROM schedules WHERE datelastaccessed < :ninetyDaysAgo";
+$pdo = dbConnection();
+$stmt = $pdo->prepare($query);
+$stmt->bindParam(":ninetyDaysAgo", $ninetyDaysAgo);
+
+if(!$stmt->execute()) {
     echo("*** Failed to run pruning query:\n");
-    echo(mysql_error() . "\n");
+    echo($stmt->errorInfo() . "\n");
 } else {
-    echo("... " . mysql_affected_rows($dbConn) . " schedules deleted\n");
+    echo("... " . $stmt->rowCount() . " schedules deleted\n");
 }
 
 ?>
